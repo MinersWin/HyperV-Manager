5e1076b90509f06d48c1b623f1d1df6e7ac0c70c
MinersWin <minerswin@minerswin.de>
2019-09-12 15:13:07 +0200

    New Update
    Police Station cannot be destroyed by an crossbow anymore!

--- HyperV-Manager.ps1
+++ HyperV-Manager.ps1
@@ -38,7 +38,7 @@
 
     .LIZENZ
       Es ist Verboten diese Dateien Weiterzuverkaufen, als Eigenwerk auszugeben oder zu Lizenzieren.
-      Es ist Erlaubt Teile des Codes bzw. Dateien für eigene Projekte zu Übernehmen, diese Projekte dÜrfen jedoch keinen Kommerziellen Nutzen haben, mÜssen unter gleichen Bedingungen weitergegeben werden und Minerswin muss als Urheber mitgenannt werden.
+      Es ist Erlaubt Teile des Codes bzw. Dateien für eigene Projekte zu übernehmen, diese Projekte dürfen jedoch keinen Kommerziellen Nutzen haben, müssen unter gleichen Bedingungen weitergegeben werden und Minerswin muss als Urheber mitgenannt werden.
       Das Projekt unterliegt der Creative Commons Lizenz 4.0 mit "by", "nc" und "sa" Modulen, d.h. Der Name des Urhebers muss genannt werden, Kommerzielle Nutzung ist Verboten, die Weitergabe muss unter den Selben bedingungen geschehen. 
 
     .PROBLEMBEHANDLUNG
@@ -1866,7 +1866,7 @@ $AboutToolStripMenuItem.Add_Click({ About })
 $Button6.Add_Click{( Erstelle-VM )}
 $button2.Add_Click{( Get-BootISOFileName_2 )}
 $PictureBox1.Add_Click{( Connect-VM )}
-$Label38.Add_Click{( Invoke-Expression "$($MyDir)\Thumbnails\" <#?ffnet Ordner beim Klick#> )}
+$Label38.Add_Click{( Invoke-Expression "$($MyDir)\Thumbnails\" <#Öffnet Ordner beim Klick#> )}
 $Button20.Add_Click{( Grant-Permission )}
 
 #Panel unsichtbar machen
@@ -1880,7 +1880,7 @@ $Panel9.Enabled = $False #Virtuellen Switch erstellen
 $Panel1.Enabled = $False #VM Erstellen
 $Panel8.Enabled = $False #Info
 
-#Kn˜pfe Unsichtbar machen
+#Knöpfe Unsichtbar machen
 $Button24.Enabled = $false
 $Button26.Enabled = $false
 
@@ -1895,7 +1895,7 @@ $RadioButton1.Checked = $true
 . InitializeComponent
 #
 #_____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
-#Funktion f?r BaloonTips
+#Funktion für BaloonTips
 Add-Type -AssemblyName  System.Windows.Forms 
 $global:balloon = New-Object System.Windows.Forms.NotifyIcon 
 Get-Member -InputObject  $Global:balloon 
@@ -1923,7 +1923,7 @@ if (Test-Path $MyDir\Log\Latest.log){
     Write-Output ChickenHung
 }
 
-#Log schreiben bei Start Log mit Datum Speichern, welches in der ersten Zeile stehen wird und danach gel?scht und neu Bef?llt wird Mfg Hullos
+#Log schreiben bei Start Log mit Datum Speichern, welches in der ersten Zeile stehen wird und danach gelöscht und neu Befüllt wird Mfg Hullos
 
 Write-Output (("{0:yyyy-MM-dd-HH-mm-ss}" -f (get-date)).ToString()) >> $MyDir\Log\Latest.log
 Write-Output "" >> $MyDir\Log\Latest.log
@@ -1970,24 +1970,24 @@ Write-Host "|/       \||/     \||/    |_||/     \||_______||_______/|/   \__/"
 
 Write-Host "Look behind this Window!"
 
-#F?gt Domain\Name in die Rechtevergabe ein
+#Fügt Domain\Name in die Rechtevergabe ein
 $TextBox13.Text = "$($env:UserDomain)\$($env:UserName)"
 Write-Host "$($env:UserDomain)\$($env:UserName)"
 
-#?berpr?ft die Version
+#Überprüft die Version
 function Test-Version{
-    #L?dt den Inhalt der Pastebin Seite mit der neusten Version
+    #Lädt den Inhalt der Pastebin Seite mit der neusten Version
     $Update = Invoke-WebRequest https://pastebin.com/raw/zBUriG1D
 
     $Update.Content
     if ($Update.Content -gt "1.9.12"){
-        #Wenn Version ?lter ist, als die aktuellte Gib Meldung aus
+        #Wenn Version älter ist, als die aktuellte Gib Meldung aus
         Write-Host "Update Erforderlich: https://pastebin.com/TvfCY9KQ"
         Write-Output "$(Get-Date) Es ist ein Update erforderlich." >> $MyDir\Log\Latest.log
     [System.Windows.Forms.MessageBox]::Show("Update ist Erforderlich, Bitte Update das Skript um die neusten Funktionen und Support zu erhalten. Alte Versionen werden nicht mehr Supportet. Aktuellste Version: https://pastebin.com/TvfCY9KQ","MinersWin Hyper-V Manager V.2",1)
     } elseif ($Update.Content -lt "1.9.12") {
         #Wenn Version neuer ist als aktuellste Gib Fehler aus
-        Write-Output "$(Get-Date) Fehler bei der Update?berprofung, Aktuelle Version ist ?lter als die Online zur verf?gung stehende." >> $MyDir\Log\Latest.log
+        Write-Output "$(Get-Date) Fehler bei der Updateüberprüfung, Aktuelle Version ist älter als die Online zur verfügung stehende." >> $MyDir\Log\Latest.log
         Write-Host "Fehler"
     [System.Windows.Forms.MessageBox]::Show("Fehler beim Abruf der aktuellsten Version","MinersWin Hyper-V Manager V.2",1)
     } else {
@@ -1997,17 +1997,17 @@ function Test-Version{
     }
 }
 #Gib Version und Build im log aus
-Write-Output "$(Get-Date) Version 1.9.13 wird ausgef?hrt. Build: 10.07.2019" >> $MyDir\Log\Latest.log
+Write-Output "$(Get-Date) Version 1.9.13 wird ausgeführt. Build: 10.07.2019" >> $MyDir\Log\Latest.log
 $balloon.BalloonTipText  = 'Version 1.9.13 wird genutzt. Build: 10.07.2019'
 $balloon.BalloonTipTitle  = "Achtung  $Env:USERNAME" 
 $balloon.Visible  = $true 
 $balloon.ShowBalloonTip(20) 
-#F?hre Versionstest aus
+#Führe Versionstest aus
 Test-Version
 
 
 
-#Pr?fe auf Adminrechte
+#Prüfe auf Adminrechte
 function Test-IsAdmin {
     try {
         Write-Output "$(Get-Date) Suche nach Adminrechten" >> $MyDir\Log\Latest.log
@@ -2015,12 +2015,12 @@ function Test-IsAdmin {
         $principal = New-Object Security.Principal.WindowsPrincipal -ArgumentList $identity
         return $principal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )
     } catch {
-        Write-Output "$(Get-Date) Fehler beim ?berpr?fen der Adminrechten" >> $MyDir\Log\Latest.log
+        Write-Output "$(Get-Date) Fehler beim überprüfen der Adminrechten" >> $MyDir\Log\Latest.log
         throw "Fehler beim erstellen der Benutzerrechte. Error: '{0}'." -f $_
     }
   }
  #
-  #?berpr?ft Admin rechte
+  #Überprüft Admin rechte
 if (-not(Test-IsAdmin)) {
     Write-Output "$(Get-Date) Programm wurde ohne Adminrechte gestartet" >> $MyDir\Log\Latest.log
     $LabelOverview.text = "Keine Adminrechte!"
@@ -2038,11 +2038,11 @@ function Load-ComboBox-VMs{ #VM Auswahl
             Write-Output "" >> $MyDir\Log\Latest.log
             Write-Output "$(Get-Date) [Lade VMs]" >> $MyDir\Log\Latest.log
             $ComboBox1.Items.Clear()
-            Write-Output "$(Get-Date) L?sche VMs aus der Liste" >> $MyDir\Log\Latest.log
+            Write-Output "$(Get-Date) Lösche VMs aus der Liste" >> $MyDir\Log\Latest.log
     $MgrArray = Get-VM -ComputerName $Hostname |
     Select-Object Name
             Write-Output "$(Get-Date) Lade alle Virtuellen Maschinen $($MgrArray)" >> $MyDir\Log\Latest.log
-            Write-Output "$(Get-Date) F?lle ComboBox1 mit VMNamen" >> $MyDir\Log\Latest.log
+            Write-Output "$(Get-Date) Fülle ComboBox1 mit VMNamen" >> $MyDir\Log\Latest.log
             Write-Output "$(Get-Date) _" >> $MyDir\Log\Latest.log
     ForEach ($item in $MgrArray) {
         $ComboBox1.Items.Add($item.Name)
@@ -2055,9 +2055,9 @@ function Load-ComboBox-Hosts{
             Write-Output "$(Get-Date) [Lade Hosts]" >> $MyDir\Log\Latest.log
             Write-Output "$(Get-Date) Lade Server mit dem Betriebsystem Windows Server in der Active Diretory" >> $MyDir\Log\Latest.log
     $ComboBox4.Items.Clear()
-            Write-Output "$(Get-Date) L?sche Host Liste" >> $MyDir\Log\Latest.log
+            Write-Output "$(Get-Date) Lösche Host Liste" >> $MyDir\Log\Latest.log
     $ComboBox4.Items.Add("localhost")
-            Write-Output "$(Get-Date) F?ge localhost zur Hostliste hinzu" >> $MyDir\Log\Latest.log
+            Write-Output "$(Get-Date) Füge localhost zur Hostliste hinzu" >> $MyDir\Log\Latest.log
     $HostArray = Get-ADComputer -Filter "OperatingSystem -like '*Server*'" | Select-Object Name
             Write-Output "$(Get-Date) Lade alle Hyper-V Hosts $($HostArray)" >> $MyDir\Log\Latest.log
             Write-Output "$(Get-Date) _" >> $MyDir\Log\Latest.log
@@ -2067,17 +2067,17 @@ function Load-ComboBox-Hosts{
         Write-Output "$(Get-Date) Fertig" >> $MyDir\Log\Latest.log
 }
 
-$RichTextBox1.Text = "Bitte einen Host ausw?hlen"
-            Write-Output "$(Get-Date) Gebe aus: Bitte einen Host ausw?hlen" >> $MyDir\Log\Latest.log
+$RichTextBox1.Text = "Bitte einen Host auswählen"
+            Write-Output "$(Get-Date) Gebe aus: Bitte einen Host auswählen" >> $MyDir\Log\Latest.log
 
 
-#Setzt die gew?hlte VM als VM und listet Infos auf
+#Setzt die gewählte VM als VM und listet Infos auf
 function Set-VM{
             Write-Output "" >> $MyDir\Log\Latest.log
             Write-Output "$(Get-Date) [Set-VM]" >> $MyDir\Log\Latest.log
     switch ($ComboBox1.Text){
-        VM {[System.Windows.Forms.MessageBox]::Show("Bitte eine VM ausw?hlen","MinersWin HyperV Manager",1)
-            Write-Output "$(Get-Date) Keine VM Ausgew?hlt" >> $MyDir\Log\Latest.log}
+        VM {[System.Windows.Forms.MessageBox]::Show("Bitte eine VM auswählen","MinersWin HyperV Manager",1)
+            Write-Output "$(Get-Date) Keine VM Ausgewählt" >> $MyDir\Log\Latest.log}
         default {
             $VMName = $ComboBox1.Text
             $Hostname = $ComboBox4.Text
@@ -2111,7 +2111,7 @@ function Set-Host{
     Write-Output "" >> $MyDir\Log\Latest.log
     Write-Output "$(Get-Date) [Set-Host]" >> $MyDir\Log\Latest.log
     switch ($ComboBox4.Text){
-        Host {[System.Windows.Forms.MessageBox]::Show("Bitte einen Host ausw?hlen","MinersWin HyperV Manager",1)}
+        Host {[System.Windows.Forms.MessageBox]::Show("Bitte einen Host auswählen","MinersWin HyperV Manager",1)}
         localhost {$Hostname = "localhost"
                 $VMs = Get-VM -ComputerName $Hostname -Name * | select VMName | Out-String
                 $RichTextBox1.Text = $VMs
@@ -2139,7 +2139,7 @@ function Set-Host{
     $Button26.Enabled = $false
     Get-Switch
     Load-ComboBox-VMs
-        "Bitte eine VM ausw?hlen"
+        "Bitte eine VM auswählen"
 }
 #Snapshots erstellen
 function Show-Snapshot{
@@ -2218,7 +2218,7 @@ function Set-ISO{
     $ISOgewaehlt = $ComboBox3.SelectedItem
     $eigeneiso = $TextBox8.Text.ToString()
     if ($ISOgewaehlt -eq "ISO Liste" -and $eigeneiso -eq ""){
-        [System.Windows.Forms.MessageBox]::Show("Bitte eine ISO ausw˜hlen","MinersWin HyperV Manager",1)
+        [System.Windows.Forms.MessageBox]::Show("Bitte eine ISO auswählen","MinersWin HyperV Manager",1)
     } elseif ($ISOgewaehlt -ne "ISO Liste" -and $eigeneiso -ne ""){
         $isopfad = $eigeneiso
     } elseif ($ISOgewaehlt -ne "ISO Liste"){
@@ -2320,7 +2320,7 @@ function Get-Switch{
 function Fill-Edit{
     Write-Output "[Fill-Edit]" >> $MyDir\Log\Latest.log
         $Datum = Get-Date
-        Write-Output "$Datum F?lle Textfelder aus" >> $MyDir\Log\Latest.log
+        Write-Output "$Datum Fülle Textfelder aus" >> $MyDir\Log\Latest.log
     $VMName = $ComboBox1.Text
     $Hostname = $ComboBox4.Text
     $CPU = Get-VM -ComputerName $Hostname -Name $VMName | Select ProcessorCount
@@ -2342,7 +2342,7 @@ function Fill-Edit{
     $TextBox11.Text = $Memory
     $TextBox12.Text = $VMName
         $Datum = Get-Date
-        Write-Output "$Datum L?sche Textfelder und ?berpr?fe Werte" >> $MyDir\Log\Latest.log
+        Write-Output "$Datum Lösche Textfelder und überprüfe Werte" >> $MyDir\Log\Latest.log
 
     $Generation = Get-VM -ComputerName $Hostname $VMName | Select Generation
     if ($Generation.Generation -eq "2"){
@@ -2440,7 +2440,7 @@ function Get-Thumbnail{
     $xRes = 640
     $yRes = 480
             $Datum = Get-Date
-            Write-Output "$Datum Erzeuge ein Screenshot der gew?hlten VM und speicher ihn ab" >> $MyDir\Log\Latest.log
+            Write-Output "$Datum Erzeuge ein Screenshot der gewählten VM und speicher ihn ab" >> $MyDir\Log\Latest.log
 
     $VMManagementService = Get-WmiObject -class "Msvm_VirtualSystemManagementService" -namespace "root\virtualization\V2" -ComputerName $HyperVParent
     $Vm = Get-WmiObject -Namespace "root\virtualization\V2" -ComputerName $HyperVParent -Query "Select * From Msvm_ComputerSystem Where ElementName='$HyperVGuest'"
@@ -2474,7 +2474,7 @@ function Connect-VM{
     $VMName = $ComboBox1.Text
     $Hostname = $ComboBox4.Text
             $Datum = Get-Date
-            Write-Output "$Datum Stelle Verbindung mit VM her und ?ffne VMConnect.exe" >> $MyDir\Log\Latest.log
+            Write-Output "$Datum Stelle Verbindung mit VM her und öffne VMConnect.exe" >> $MyDir\Log\Latest.log
     vmconnect.exe $Hostname $VMName /edit
             $Datum = Get-Date
             Write-Output "$Datum Fertig" >> $MyDir\Log\Latest.log
@@ -2486,11 +2486,11 @@ function Connect-VM{
 function Open-Manager{
         Write-Output "[Open-Manager]" >> $MyDir\Log\Latest.log
             $Datum = Get-Date
-            Write-Output "$Datum ?ffne den Hyper-V Manager" >> $MyDir\Log\Latest.log
+            Write-Output "$Datum Öffne den Hyper-V Manager" >> $MyDir\Log\Latest.log
     virtmgmt.msc
             $Datum = Get-Date
             Write-Output "$Datum Fertig" >> $MyDir\Log\Latest.log
-            $balloon.BalloonTipText  = "Der Hyper-V Manager wurde ge?ffnet!"
+            $balloon.BalloonTipText  = "Der Hyper-V Manager wurde geöffnet!"
             $balloon.Visible  = $true 
             $balloon.ShowBalloonTip(20)
 }
@@ -2605,7 +2605,7 @@ function Erstelle-VM{
     }
 
 
-    #  F?ge DVD Drive hinzu und binde es ein
+    #  Füge DVD Drive hinzu und binde es ein
     if ($ISO_der_VM -ne "") {Get-VM -ComputerName $Hostname $Name_Der_VM | Add-VMDvdDrive -Path $ISO_der_VM}
 
     $CDriveVHDPath=$VM_Root_Path+"\"+$Name_Der_VM+"\"+$Name_Der_VM+"-C.vhdx"
@@ -2699,7 +2699,7 @@ function Send-Feedback{
     $LabelSender.Font                = 'Comic Sans MS,10'
 
     $LabelTitle                      = New-Object system.Windows.Forms.Label
-    $LabelTitle.text                 = "Hier kannst du dein Feedback und Verbesserungsvorschl?ge"
+    $LabelTitle.text                 = "Hier kannst du dein Feedback und Verbesserungsvorschläge"
     $LabelTitle.AutoSize             = $true
     $LabelTitle.width                = 25
     $LabelTitle.height               = 10
