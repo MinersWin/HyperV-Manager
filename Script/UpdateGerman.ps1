f5ec9eb7945c34c56801d229c3181240f32d0a96
MinersWin <minerswin@minerswin.de>
2019-11-12 11:26:45 +0100

    New German Language Script

---
+++ Script/UpdateGerman.ps1
@@ -0,0 +1,38 @@
+#Deutsche Sprachdatei
+#Update: 12.11.2019
+
+#
+# Hostname
+#
+$LabelOverview.Text = "Servername"
+$Button13.Text = "Neu laden"
+$Button14.Text = "Weiter"
+$RichTextBox1.Text = "Bitte einen Host auswählen"
+
+#
+# VM settings
+#
+$Label41.Text = "Virtuelle Maschine"
+$Button1.Text = "Weiter"
+$Button24.Text = "Mit VM Verbinden"
+$Button25.Text = "Öffne HyperV-Manager"
+$Button26.Text = "Aktualisiere Vorschau"
+$Button27.Text = "Aktualisiere Vorschau"
+$Button28.Text = "Log Speichern"
+$Button29.Text = "Aktualisieren"
+
+$Label11.Text = "Start/Stop"
+$Button8.Text = "VM Starten"
+$Button9.Text = "VM Stoppen"
+
+$Label8.Text = "Snapshot erstellen"
+$Button5.Text = "Zeige Snapshots"
+$Button4.Text = "Erstellen"
+
+$Label13.Text = "ISO einlegen"
+$Label14.Text = "Eigene ISO"
+$Button10.Text = "Suchen"
+$Button11.Text = "Zurücksetzen"
+$Button12.Text = "Weiter"
+
+$Label26.Text = "VM Bearbeiten"
